6fc91099b330da1a0994edd18ce63bb05b8399b4
Artyushenko Daniil <foxones80@gmail.com>
2025-06-18 02:48:40 +0600

    added some res

--- src/js/_components.js
+++ src/js/_components.js
@@ -4,6 +4,7 @@ import { initHeader } from "./components/header.js";
 import { initAllSliders } from "./components/slider.js";
 import { initInfraCards } from "./components/infra.js";
 import { initModals } from "./components/modal.js";
+import { burger } from "./functions/burger.js";
 initHeader();
 initAllSliders();
 initInfraCards();
